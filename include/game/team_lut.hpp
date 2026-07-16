e410db86029454e6bbf3be2fdfa01d69ca9adec7
startfliing <jbhelgas@gmail.com>
2026-07-16 12:15:02 -0700

    Add intial Madden Data

---
+++ include/game/team_lut.hpp
@@ -0,0 +1,9 @@
+#ifndef __TEAM_LUT__
+#define __TEAM_LUT__
+
+#define MAX_TEAM_NAME_LENGTH 22
+#define MAX_PLAYERS_PER_TEAM 74
+
+extern const char* team_lut[33];
+
+#endif
--- source/states/title.cpp
+++ source/states/title.cpp
@@ -4,13 +4,55 @@
 #include "title.hpp"
 #include "terminal.hpp"
 #include "pregame.hpp"
+#include "players.h"
+#include "team_lut.hpp"
+
+void logTeam(int team, int player){
+    Terminal::reset();
+    Terminal::log("View Teams");
+    Terminal::log(team_lut[team]);
+    int tempPlayer = 0;
+    for(int i = 0; i < players_count; i++){
+        if(players_data[i].team_id != team){
+            continue;
+        }
+
+        if(player != tempPlayer){
+            tempPlayer++;
+            continue;
+        }else{
+            Terminal::log("#%% %% %%, %%", 
+                players_data[i].jersey_number, 
+                players_data[i].first_name, 
+                players_data[i].last_name,
+                players_data[i].position_short_label
+            );
+            break;
+        }
+    }
+
+}
 
 GameState titleState(){
     REG_DISPCNT = Terminal::initTerminal() | DCNT_MODE0;
 
-    Terminal::log("title state");
+    
 
-    while(!key_hit(KEY_ANY)){
+    int teamNum = 1;
+    int currPlayerInd = 0;
+    logTeam(teamNum, currPlayerInd);
+    while(!key_hit(KEY_START)){
+        if(key_hit(KEY_SHOULDER)){
+            teamNum = wrap(teamNum + key_tri_shoulder(), 1, 33);
+            currPlayerInd = 0;
+            logTeam(teamNum, currPlayerInd);
+        }
+
+        if(key_hit(KEY_FIRE)){
+            currPlayerInd = wrap(currPlayerInd + key_tri_fire(), 0, MAX_PLAYERS_PER_TEAM);
+            logTeam(teamNum, currPlayerInd);
+        }
+        
         key_poll();
         VBlankIntrWait();
     }
