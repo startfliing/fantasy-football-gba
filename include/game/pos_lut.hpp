977d48ffc32ced93bcd4f8d73826a3484f054480
startfliing <jbhelgas@gmail.com>
2026-07-28 15:25:33 -0700

    add pos enum

--- include/game/pos_lut.hpp
+++ include/game/pos_lut.hpp
@@ -3,4 +3,29 @@
 
 extern const char* positions[21];
 
+// Indices into positions[], matches pos_id in players.csv/players_data
+enum PosId{
+    POS_NA   = 0,
+    POS_C    = 1,
+    POS_CB   = 2,
+    POS_DT   = 3,
+    POS_RB   = 4,
+    POS_S    = 5,
+    POS_K    = 6,
+    POS_LE   = 7,
+    POS_LG   = 8,
+    POS_LOLB = 9,
+    POS_LS   = 10,
+    POS_LT   = 11,
+    POS_MLB  = 12,
+    POS_P    = 13,
+    POS_QB   = 14,
+    POS_RE   = 15,
+    POS_RG   = 16,
+    POS_ROLB = 17,
+    POS_RT   = 18,
+    POS_TE   = 19,
+    POS_WR   = 20
+};
+
 #endif
